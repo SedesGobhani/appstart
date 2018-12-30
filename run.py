5dab504a8c1c20b2f90eef8dace699f99e67dfb5
Mark Seagoe <mark.a.seagoe@intel.com>
2018-12-30 00:17:09 -0800

    Made menu and title 2 separate render_template args.

--- run.py
+++ run.py
@@ -9,13 +9,13 @@ menu = [
       {'name':'Log Out', 'href':'#'}
 	]
 
-docbag = {"title": "Flask/JS App", "menu": menu}
+title = "Flask/JS App"
 
 app = Flask(__name__)
 
 @app.route ('/')
 def home():
-    return render_template("home.html", docbag=docbag)
+    return render_template("home.html", title=title, menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
