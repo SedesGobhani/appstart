00782884565d9c521cd0a9f8c8152f10cc7dbfab
Mark Seagoe <mark.a.seagoe@intel.com>
2018-12-31 13:11:18 -0800

    Pushed header colors from Python ==> Javascript.

--- run.py
+++ run.py
@@ -1,21 +1,43 @@
 
 from flask import Flask, render_template, jsonify
 
-menu = [
-      {'name':'Home', 'href':'#'},
-      {'name':'About', 'href':'#'},
-      {'name':'Forum', 'href':'#'},
-      {'name':'Contact', 'href':'#'},
-      {'name':'Log Out', 'href':'#'}
-	]
+links = [
+    {'name':'Home', 'href':'#'},
+    {'name':'About', 'href':'#'},
+    {'name':'Forum', 'href':'#'},
+    {'name':'Contact', 'href':'#'},
+    {'name':'Log Out', 'href':'#'}
+]
+
+colors = {
+    'background': '#000040',
+    'foreground': '#6495ED',
+    'button_background': '#000040',
+    'button_foreground': '#6495ED',
+    'button_border': '#6495ED',
+    'selbtn_background': '#000040',
+    'selbtn_foreground': 'tomato',
+    'selbtn_border': 'tomato',
+    'hvrbtn_background': '#000040',
+    'hvrbtn_foreground': 'tomato',
+    'hvrbtn_border': '#6495ED'
+}
+
+header = {"links":links, "colors": colors}
+
 
 title = "Flask/JS App"
+
+head = {
+    'title': title
+}
+
 
 app = Flask(__name__)
 
 @app.route ('/')
 def home():
-    return render_template("home.html", title=title, menu=menu)
+    return render_template("home.html", head=head, header=header)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,34 +1,65 @@
 
   $(document).ready(function(){
 
-    //console.log(menuItems); // debug
+    //console.log(menu); // debug
+    console.log(colors); // debug
+
+    // insert menu into DOM
     var n;
-    for (n=0; n<menuItems.length; n++) {
-      var newlink = "<a href=" + menuItems[n].href + ">" + menuItems[n].name + "</a>";
+    for (n=0; n<menu.length; n++) {
+      var newlink = "<a href=" + menu[n].href + ">" + menu[n].name + "</a>";
       $("nav").append(newlink);
     }
+    // initial settings for header
+    $("header").css("background-color", colors.background);
+    $("header").css("color", colors.foreground);
+    // initial settings for menu items
+    $("a").each(function(i) {
+      $(this).css("color", colors.button_foreground);
+      $(this).css("background", colors.button_background);
+      $(this).css("border-style", "solid");
+      $(this).css("border-width", "1px");
+      $(this).css("border-color", colors.button_border);
+    });
 
+    // menu events drive css
+    // for each button
     $( "a" ).each(function(i) {
 
+      // establish click functions
       $(this).click(function() {  // click occurred
+        // clear previously selected button
         $(this).siblings().each(function(i) {
-          $(this).css("border", "");
-          $(this).data("selected", false);
-          $(this).css("color", "");
-        })
-        $(this).css("border", "1px solid tomato");
+          if( $(this).data("selected") ) {
+            $(this).data("selected", false);
+            $(this).css("color", colors.button_foreground);
+            $(this).css("background", colors.button_background);
+            $(this).css("border-color", colors.button_border);
+          }
+        });
+        // set this selected button
         $(this).data("selected", true);
-      });
+        $(this).css("color", colors.selbtn_foreground);
+        $(this).css("background", colors.selbtn_background);
+        $(this).css("border-color", colors.selbtn_border);
+      })
 
+    // establish hover functions
     $(this).hover(
       function() { // hover activated
-        $(this).css("color", "tomato");
+        $(this).css("color", colors.hvrbtn_foreground);
+        $(this).css("background", colors.hvrbtn_background);
+        $(this).css("border-color", colors.hvrbtn_border);
       },
       function(){ // hover deactivated
         if( $(this).data("selected") ) {
-          $(this).css("color", "tomato");
+          $(this).css("color", colors.selbtn_foreground);
+          $(this).css("background", colors.selbtn_background);
+          $(this).css("border-color", colors.selbtn_border);
         } else {
-          $(this).css("color", "");
+          $(this).css("color", colors.button_foreground);
+          $(this).css("background", colors.button_background);
+          $(this).css("border-color", colors.button_border);
         }
       });
 
