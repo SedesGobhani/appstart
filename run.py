f0ea0ec01e4ca6ea55e120f46b33a95980bcc1a2
Mark Seagoe <mark.a.seagoe@intel.com>
2018-12-31 20:00:05 -0800

    Added About page. Nav bar navigates! Menu has initial page selected.

--- run.py
+++ run.py
@@ -2,29 +2,30 @@
 from flask import Flask, render_template, jsonify
 
 links = [
-    {'name':'Home', 'href':'#'},
-    {'name':'About', 'href':'#'},
+    {'name':'Home', 'href':'/home'},
+    {'name':'About', 'href':'/about'},
     {'name':'Forum', 'href':'#'},
     {'name':'Contact', 'href':'#'},
     {'name':'Log Out', 'href':'#'}
 ]
 
 colors = {
-    'background': '#000040',
     'foreground': '#6495ED',
-    'button_background': '#000040',
+    'background': '#000040',
     'button_foreground': '#6495ED',
-    'button_border': '#6495ED',
-    'selbtn_background': '#000040',
+    'button_background': '#000040',
+    'button_border': '#000040',
     'selbtn_foreground': 'tomato',
-    'selbtn_border': 'tomato',
+    'selbtn_background': '#000040',
+    'selbtn_border': '#000040',
+    'hvrbtn_foreground': '#6495ED',
     'hvrbtn_background': '#000040',
-    'hvrbtn_foreground': 'tomato',
-    'hvrbtn_border': '#6495ED'
+    'hvrbtn_border': 'tomato'
 }
 
-header = {"links":links, "colors": colors}
+misc = {'selected': 'Home'}
 
+header = {"links":links, "colors": colors, "misc": misc}
 
 title = "Flask/JS App"
 
@@ -32,12 +33,18 @@ head = {
     'title': title
 }
 
-
 app = Flask(__name__)
 
-@app.route ('/')
+@app.route('/')
+@app.route ('/home')
 def home():
+    header['misc']['selected'] = 'Home';
     return render_template("home.html", head=head, header=header)
+
+@app.route ('/about')
+def about():
+    header['misc']['selected'] = 'About';
+    return render_template("about.html", head=head, header=header)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- static/js/header.js
+++ static/js/header.js
@@ -1,32 +1,49 @@
+    var menu = header.links;
+    var colors = header.colors;
+    var selected = header.misc.selected;
+
+    console.log(header); // debug
+    console.log("Selected = " + selected);
 
   $(document).ready(function(){
-
-    //console.log(menu); // debug
-    console.log(colors); // debug
 
     // insert menu into DOM
     var n;
     for (n=0; n<menu.length; n++) {
       var newlink = "<a href=" + menu[n].href + ">" + menu[n].name + "</a>";
       $("nav").append(newlink);
+      if( menu[n].name == selected ) {
+        $("a").last().data("selected", true);
+      } else {
+        $("a").last().data("selected", false);
+      }
     }
+
     // initial settings for header
     $("header").css("background-color", colors.background);
     $("header").css("color", colors.foreground);
+
     // initial settings for menu items
     $("a").each(function(i) {
-      $(this).css("color", colors.button_foreground);
-      $(this).css("background", colors.button_background);
       $(this).css("border-style", "solid");
       $(this).css("border-width", "1px");
-      $(this).css("border-color", colors.button_border);
+      if( $(this).data("selected") ) { // this page is selected
+        $(this).css("color", colors.selbtn_foreground);
+        $(this).css("background", colors.selbtn_background);
+        $(this).css("border-color", colors.selbtn_border);
+      } else {
+        $(this).css("color", colors.button_foreground);
+        $(this).css("background", colors.button_background);
+        $(this).css("border-color", colors.button_border);
+      }
     });
 
-    // menu events drive css
+
+    // menu events that drive css
     // for each button
     $( "a" ).each(function(i) {
 
-      // establish click functions
+      // establish "click" functions
       $(this).click(function() {  // click occurred
         // clear previously selected button
         $(this).siblings().each(function(i) {
@@ -44,12 +61,14 @@
         $(this).css("border-color", colors.selbtn_border);
       })
 
-    // establish hover functions
+    // establish "hover" functions
     $(this).hover(
       function() { // hover activated
-        $(this).css("color", colors.hvrbtn_foreground);
-        $(this).css("background", colors.hvrbtn_background);
-        $(this).css("border-color", colors.hvrbtn_border);
+        if( !$(this).data("selected") ) {
+          $(this).css("color", colors.hvrbtn_foreground);
+          $(this).css("background", colors.hvrbtn_background);
+          $(this).css("border-color", colors.hvrbtn_border);
+        }
       },
       function(){ // hover deactivated
         if( $(this).data("selected") ) {
